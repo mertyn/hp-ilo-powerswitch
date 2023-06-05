726f1e25046a190cb50fdd483067ec3f802f9a20
mertyn <mweber28@hs-mittweida.de>
2023-06-05 20:41:29 +0200

    Started adding client frontend

---
+++ public/index.js
@@ -0,0 +1,10 @@
+function $(selector) {
+    return document.querySelector(selector)
+}
+
+const buttonSwitch = $("#switch")
+
+
+buttonSwitch.addEventListener("click", () => {
+
+})
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
+import express, { Application } from "express"
 
-const { Client } = require("ssh2");
+const { Client } = require("ssh2")
 
 const config = {
     host: process.env.HOST,
@@ -13,19 +14,21 @@ const config = {
     }
 }
 
-
 const conn = new Client();
-conn.on('ready', () => {
-    console.log('Client :: ready');
-    conn.exec('power', (err: any, stream: any) => {
-        if (err) throw err;
-        stream.on('close', (code: any, signal: any) => {
-            console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-            conn.end();
-        }).on('data', (data: any) => {
-            console.log('STDOUT: ' + data);
-        }).stderr.on('data', (data: any) => {
-            console.log('STDERR: ' + data);
-        });
-    });
-}).connect(config);
+
+// conn.on("ready", () => {
+//     console.log('Client :: ready');
+// })
+// .on("end", () => {
+//     console.log('Client :: end');  
+// })
+// .connect(config);
+
+
+const app = express()
+const port = process.env.WEBPORT || 5000;
+
+app.use(express.static("public"))
+app.listen(port, () => {
+    return console.log(`Webserver is listening at http://localhost:${port}`);
+})
