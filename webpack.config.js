2ae650154e79d9ff4efc02b3508dd53cbdd9d5eb
mertyn <mweber28@hs-mittweida.de>
2023-06-09 23:29:00 +0200

    Added sass and restructured build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './client/index.ts', // Update with your client-side entry file path
+    entry: ['./client/ts/index.ts', './client/scss/style.scss'], // Update with your client-side entry file path
     output: {
         path: path.resolve(__dirname, 'public'), // Update with your desired output directory
         filename: 'bundle.js',
+        publicPath: 'public/',
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js'],
@@ -17,6 +19,17 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
         ],
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: 'client/index.html', to: 'index.html' },
+            ],
+        }),
+    ],
 };
