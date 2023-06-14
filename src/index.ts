b2b253970ac044f978d2a23d6a4d9bfd8befdf08
mertyn <mweber28@hs-mittweida.de>
2023-06-14 22:34:02 +0200

    Added test request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import express from "express"
+import https from "https"
 import http from "http"
 import { Server } from "socket.io"
 
@@ -20,6 +21,32 @@ io.on("connection", (socket) => {
     })
 })
 
-server.listen(port, () => {
-    return console.log(`Webserver is listening at http://localhost:${port}`);
+// server.listen(port, () => {
+//     return console.log(`Webserver is listening at http://localhost:${port}`);
+// })
+
+
+const iloIP = process.env.HOST;
+const username = process.env.USER;
+const password = process.env.PASS;
+
+const options = {
+    hostname: iloIP,
+    path: "/redfish/v1/Systems/1/",
+    method: "GET",
+    auth: `${username}:${password}`,
+    rejectUnauthorized: false
+}
+
+const req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on("data", (data) => {
+        console.log(data.toString())
+    })
+
+}).setTimeout(100000).on("error", (error) => {
+    console.error('Error:', error)
 })
+
+req.end()
